851c28d7860c63528ccebf37f845bbdda557dec2
LeslieWong <99999r00t@gmail.com>
2016-07-16 22:41:02 +0800

    update details

--- resources/views/admin/index/index.blade.php
+++ resources/views/admin/index/index.blade.php
@@ -1,1 +1,11 @@
 @extends('common.admin.layout')
+@section('css')
+    <style>
+        body {
+            font-family: 'monospace', 微软雅黑;
+            /*font-size: 9px;*/
+        }
+
+    </style>
+@endsection
+
